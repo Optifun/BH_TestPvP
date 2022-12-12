3177acbc77a92393c1060e10ec2a146f5494aa9b
Dmitry Frolov <frdima19@gmail.com>
2022-12-12 20:48:39 +0700

    Add RoomManager & RoomPlayer prototypes

---
+++ Assets/Scripts/Game/Lobby/Services/RoomManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirror;
+
+namespace Game.Lobby.Services
+{
+    public class RoomManager : NetworkRoomManager
+    {
+        public event Action<NetworkConnection, AuthenticationData> ClientConnected;
+        public event Action<NetworkConnection, AuthenticationData> ClientDisconnected;
+        public event Action PlayersReady;
+
+        public Dictionary<NetworkConnection, AuthenticationData> Clients { get; } = new();
+
+
+        public override void OnRoomServerConnect(NetworkConnectionToClient conn)
+        {
+            var data = conn.authenticationData as AuthenticationData;
+            Clients.Add(conn, data);
+            ClientConnected?.Invoke(conn, data);
+        }
+
+        public override void OnRoomServerDisconnect(NetworkConnectionToClient conn)
+        {
+            if (Clients.TryGetValue(conn, out var data))
+                ClientDisconnected?.Invoke(conn, data);
+        }
+
+        public override void OnRoomServerPlayersReady()
+        {
+            PlayersReady?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Lobby/Services/RoomPlayer.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+
+namespace Game.Lobby.Services
+{
+    public class RoomPlayer : NetworkRoomPlayer
+    {
+        public string Username { get; set; }
+    }
+}
