9d19f258df36b156126b69e86656a346aa27739f
Dmitry Frolov <frdima19@gmail.com>
2022-12-13 17:00:19 +0700

    Add PlayerBubble script

---
+++ Assets/Scripts/Game/Lobby/View/PlayerBubble.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Game.Lobby.Services;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Lobby.View
+{
+    public class PlayerBubble : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        private RoomPlayer _player;
+
+        public void Initialize(RoomPlayer player)
+        {
+            _player = player;
+            _player.OnReadyChanged += OnPlayerReadyChanged;
+            _player.OnUsernameChanged += OnPlayerUsernameChanged;
+        }
+
+        private void OnPlayerUsernameChanged(RoomPlayer sender, string newName) =>
+            _text.text = newName;
+
+        private void OnPlayerReadyChanged(RoomPlayer sender, bool ready) =>
+            _text.color = sender.readyToBegin ? Color.green : Color.red;
+
+
+        private void OnDestroy()
+        {
+            _player.OnReadyChanged -= OnPlayerReadyChanged;
+            _player.OnUsernameChanged -= OnPlayerUsernameChanged;
+        }
+    }
+}
