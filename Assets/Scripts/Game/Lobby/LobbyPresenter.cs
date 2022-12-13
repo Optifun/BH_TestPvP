e1f1d78d61b8a0ebeb30d204214b8da1bf8572d3
Dmitry Frolov <frdima19@gmail.com>
2022-12-13 17:03:14 +0700

    Update LobbyUI: display players

--- Assets/Scripts/Game/Lobby/LobbyPresenter.cs
+++ Assets/Scripts/Game/Lobby/LobbyPresenter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Game.Lobby.Services;
 using Game.Lobby.View;
 using Game.State;
+using Mirror;
 
 namespace Game.Lobby
 {
@@ -10,12 +12,17 @@ namespace Game.Lobby
         private readonly RoomManager _roomManager;
         private readonly LobbyUI _lobbyUI;
         private readonly GameState _gameState;
+        private RoomPlayer _localPlayer;
+        public List<NetworkRoomPlayer> Players => _roomManager.roomSlots;
 
         public LobbyPresenter(RoomManager roomManager, LobbyUI lobbyUI, GameState gameState)
         {
             _lobbyUI = lobbyUI;
             _roomManager = roomManager;
             _gameState = gameState;
+
+            _roomManager.ClientConnected += OnClientConnected;
+            _roomManager.ClientDisconnected += OnClientDisconnected;
         }
 
         public void GotoConnection()
@@ -23,14 +30,39 @@ namespace Game.Lobby
             _lobbyUI.ShowConnectionForm();
         }
 
-        public void SetUsername(string username)
+        public void HostGame()
         {
-            _gameState.LobbyState.Username = username;
+            _roomManager.StartHost();
         }
 
         public void Connect(string host)
         {
             _roomManager.StartClient(new Uri(host));
         }
+
+        public void ToggleReady()
+        {
+            _localPlayer.CmdChangeReadyState(!_localPlayer.readyToBegin);
+        }
+
+        public void SetUsername(string username)
+        {
+            _gameState.LobbyState.Username = username;
+        }
+
+        private void OnClientConnected(NetworkConnection connection, AuthenticationData _)
+        {
+            var roomPlayer = connection.identity.GetComponent<RoomPlayer>();
+            _lobbyUI.DisplayPlayer(roomPlayer);
+
+            if (NetworkClient.localPlayer == connection.identity)
+            {
+                _localPlayer = roomPlayer;
+                _lobbyUI.AttachLocalPlayer(_localPlayer);
+            }
+        }
+
+        private void OnClientDisconnected(NetworkConnection connection, AuthenticationData _) =>
+            _lobbyUI.RemovePlayer(connection.identity.GetComponent<RoomPlayer>());
     }
 }
--- Assets/Scripts/Game/Lobby/View/LobbyUI.cs
+++ Assets/Scripts/Game/Lobby/View/LobbyUI.cs
@@ -1,21 +1,106 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Game.Lobby.Services;
+using Mirror;
+using TMPro;
+using UI;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Game.Lobby.View
 {
     public class LobbyUI : MonoBehaviour
     {
-        private LobbyPresenter _presenter;
+        [SerializeField] private PlayerBubble _playerBubblePrefab;
 
-        [SerializeField] private GameObject ConnectionForm;
+        [SerializeField] private ScrollRect _playerBubblesContainer;
+        [SerializeField] private ButtonContainer _readyButton;
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _connectButton;
+        [SerializeField] private TMP_InputField _addressInput;
+
+        [SerializeField] private GameObject _connectionForm;
+
+        private Dictionary<RoomPlayer, PlayerBubble> _playersList = new();
+        private LobbyPresenter _presenter;
+        private RoomPlayer _localPlayer;
 
         public void Initialize(LobbyPresenter presenter)
         {
             _presenter = presenter;
+            _hostButton.onClick.AddListener(OnHostButtonClick);
+            _connectButton.onClick.AddListener(OnConnectButtonClick);
+            _readyButton.Button.onClick.AddListener(OnReadyClick);
+        }
+
+        private void OnReadyClick()
+        {
+            _presenter.ToggleReady();
         }
 
         public void ShowConnectionForm()
         {
-            ConnectionForm.SetActive(true);
+            _connectionForm.SetActive(true);
+        }
+
+        public void DisplayPlayers(List<RoomPlayer> players)
+        {
+            ClearPlayersList();
+
+            foreach (var roomPlayer in players)
+                DisplayPlayer(roomPlayer);
+        }
+
+
+        public void DisplayPlayer(RoomPlayer player)
+        {
+            PlayerBubble playerBubble = SpawnPlayerBubble(player);
+            _playersList.Add(player, playerBubble);
+        }
+
+        public void RemovePlayer(RoomPlayer player)
+        {
+            Destroy(_playersList[player].gameObject);
+            _playersList.Remove(player);
+        }
+
+        public void AttachLocalPlayer(RoomPlayer roomPlayer)
+        {
+            _localPlayer = roomPlayer;
+            _localPlayer.OnReadyChanged += OnLocalPlayerReadyChanged;
+        }
+
+        private void OnConnectButtonClick() =>
+            _presenter.Connect(_addressInput.text);
+
+        private void OnHostButtonClick() =>
+            _presenter.HostGame();
+
+        private void OnLocalPlayerReadyChanged(RoomPlayer _, bool ready) =>
+            _readyButton.Text.text = ready ? "Unready" : "Ready";
+
+        private PlayerBubble SpawnPlayerBubble(RoomPlayer player)
+        {
+            var playerBubble = Instantiate(_playerBubblePrefab, _playerBubblesContainer.content);
+            playerBubble.Initialize(player);
+            return playerBubble;
+        }
+
+        private void ClearPlayersList()
+        {
+            foreach (var pair in _playersList)
+                Destroy(pair.Value.gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            if (_localPlayer)
+                _localPlayer.OnReadyChanged -= OnLocalPlayerReadyChanged;
+
+            _hostButton.onClick.RemoveListener(OnHostButtonClick);
+            _connectButton.onClick.RemoveListener(OnConnectButtonClick);
+            _readyButton.Button.onClick.RemoveListener(OnReadyClick);
         }
     }
 }
