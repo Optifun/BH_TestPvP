5b86e02f35075cb638185c1d268325df8fa46dcc
Dmitry Frolov <frdima19@gmail.com>
2022-12-15 01:24:43 +0700

    Add ArenaStaticData

---
+++ Assets/Scripts/Static/ArenaStaticData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Static
+{
+    [CreateAssetMenu(menuName = "Arena Config", fileName = "ArenaConfig", order = 0)]
+    public class ArenaStaticData : ScriptableObject
+    {
+        [field: Tooltip("Charging distance"), SerializeField]
+        public float ChargeDistance { get; private set; }
+
+        [field: Tooltip("Duration of charging state"), SerializeField]
+        public float ChargingTime { get; private set; }
+
+        [field: Tooltip("Duration of player invincible state"), SerializeField]
+        public float InvincibilityDuration { get; private set; }
+
+        [field: Tooltip("Amount of required hits for each player"), SerializeField]
+        public float RequireHitCount { get; private set; }
+
+        [field: Tooltip("Delay before next match"), SerializeField]
+        public float MatchReloadDelay { get; private set; }
+    }
+}
