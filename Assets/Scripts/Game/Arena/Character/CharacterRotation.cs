7a88b4c5aee91732253b0f67303a4379b28656eb
Dmitry Frolov <frdima19@gmail.com>
2022-12-16 21:20:04 +0700

    Update CharacterRotation: sync rotation

--- Assets/Scripts/Game/Arena/Character/CharacterRotation.cs
+++ Assets/Scripts/Game/Arena/Character/CharacterRotation.cs
@@ -1,16 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using Mirror;
+using UnityEngine;
 
 namespace Game.Arena.Character
 {
-    public class CharacterRotation : MonoBehaviour
+    public class CharacterRotation : NetworkBehaviour
     {
         [SerializeField] private CharacterMovement movement;
         [SerializeField] private Transform model;
 
+        [SyncVar] private Quaternion _rotation = Quaternion.identity;
+
+        public override void OnStartClient()
+        {
+            _rotation = model.rotation;
+        }
+
         private void FixedUpdate()
         {
+            if (!isLocalPlayer)
+            {
+                model.rotation = LerpRotation(_rotation);
+                return;
+            }
+
             if (movement.Movement != Vector3.zero)
-                model.rotation = Quaternion.Lerp(model.rotation, Rotate(movement.Movement), 0.24f);
+                model.rotation = LerpRotation(Rotate(movement.Movement));
+        }
+
+        private Quaternion LerpRotation(Quaternion nextValue) =>
+            Quaternion.Lerp(model.rotation, nextValue, 0.24f);
+
+        private void Update()
+        {
+            if (isLocalPlayer)
+                _rotation = model.rotation;
         }
 
         private static Quaternion Rotate(Vector3 movementDirection)
